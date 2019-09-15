e22c4dd323f198804e519d0de3edbf02ab3f3e0d
sounisi5011 <sounisi5011@users.noreply.github.com>
2019-09-15 23:27:36 +0900

    ✅ Add a test that does not include detailed errors in the stack property

--- test/index.ts
+++ test/index.ts
@@ -26,6 +26,27 @@ test('should throw error by invalid html', async t => {
         await processAsync(metalsmith);
     });
 
-    t.regex(error.message, /^frameset\.html:$/m);
-    t.regex(error.message, /^non-utf8\.html:$/m);
+    t.regex(
+        error.message,
+        /^frameset\.html:$/m,
+        `The error object's "message" property should have an invalid HTML filenames`,
+    );
+    t.regex(
+        error.message,
+        /^non-utf8\.html:$/m,
+        `The error object's "message" property should have an invalid HTML filenames`,
+    );
+
+    if (error.stack) {
+        t.notRegex(
+            error.stack,
+            /^frameset\.html:$/m,
+            `The error object's "stack" property should not have an invalid HTML filenames`,
+        );
+        t.notRegex(
+            error.stack,
+            /^non-utf8\.html:$/m,
+            `The error object's "stack" property should not have an invalid HTML filenames`,
+        );
+    }
 });
