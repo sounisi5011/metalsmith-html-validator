c5d15507a8214348c489b92bbdbb2ad0c50d77c1
sounisi5011 <sounisi5011@users.noreply.github.com>
2019-09-19 21:33:08 +0900

    ✅ Update tests

--- test/index.ts
+++ test/index.ts
@@ -10,7 +10,13 @@ const fixtures = path.join(__dirname, 'fixtures');
 test('should not throw error by valid html files', async t => {
     const metalsmith = Metalsmith(path.join(fixtures, 'valid'))
         .source('src')
-        .use(htmlValidator());
+        .use(
+            htmlValidator({
+                // disable display to stderr
+                // eslint-disable-next-line @typescript-eslint/no-empty-function
+                logger() {},
+            }),
+        );
 
     await t.notThrowsAsync(async () => {
         await processAsync(metalsmith);
@@ -20,7 +26,13 @@ test('should not throw error by valid html files', async t => {
 test('should not throw error by valid single html file', async t => {
     const metalsmith = Metalsmith(path.join(fixtures, 'valid-single'))
         .source('src')
-        .use(htmlValidator());
+        .use(
+            htmlValidator({
+                // disable display to stderr
+                // eslint-disable-next-line @typescript-eslint/no-empty-function
+                logger() {},
+            }),
+        );
 
     await t.notThrowsAsync(async () => {
         await processAsync(metalsmith);
@@ -28,23 +40,41 @@ test('should not throw error by valid single html file', async t => {
 });
 
 test('should throw error by invalid html files', async t => {
+    let details = '';
     const metalsmith = Metalsmith(path.join(fixtures, 'invalid'))
         .source('src')
-        .use(htmlValidator());
+        .use(
+            htmlValidator({
+                logger(str) {
+                    details = str;
+                },
+            }),
+        );
 
     const error = await t.throwsAsync(async () => {
         await processAsync(metalsmith);
     });
 
     t.regex(
-        error.message,
+        details,
         /^frameset\.html:$/m,
-        `The error object's "message" property should have an invalid HTML filenames`,
+        `The logger()'s argument should have an invalid HTML filenames`,
     );
     t.regex(
+        details,
+        /^non-utf8\.html:$/m,
+        `The logger()'s argument should have an invalid HTML filenames`,
+    );
+
+    t.notRegex(
+        error.message,
+        /^frameset\.html:$/m,
+        `The error object's "message" property should not have an invalid HTML filenames`,
+    );
+    t.notRegex(
         error.message,
         /^non-utf8\.html:$/m,
-        `The error object's "message" property should have an invalid HTML filenames`,
+        `The error object's "message" property should not have an invalid HTML filenames`,
     );
 
     if (error.stack) {
@@ -62,18 +92,31 @@ test('should throw error by invalid html files', async t => {
 });
 
 test('should throw error by invalid single html file', async t => {
+    let details = '';
     const metalsmith = Metalsmith(path.join(fixtures, 'invalid-single'))
         .source('src')
-        .use(htmlValidator());
+        .use(
+            htmlValidator({
+                logger(str) {
+                    details = str;
+                },
+            }),
+        );
 
     const error = await t.throwsAsync(async () => {
         await processAsync(metalsmith);
     });
 
     t.regex(
+        details,
+        /^empty-title\.html:$/m,
+        `The logger()'s argument should have an invalid HTML filenames`,
+    );
+
+    t.notRegex(
         error.message,
         /^empty-title\.html:$/m,
-        `The error object's "message" property should have an invalid HTML filename`,
+        `The error object's "message" property should not have an invalid HTML filename`,
     );
 
     if (error.stack) {
